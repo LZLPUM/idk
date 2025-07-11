3ee32b378dd2717d478a801694f9d8c9b6ce031e
LZLPUM <vvvhhcvg56@gmail.com>
2025-07-11 19:18:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.get('/', (req, res) => {
       body {
         background: linear-gradient(#000015, #000000);
         color: #fff; font-family: sans-serif; text-align: center; padding: 20px;
-        background-image: url('https://unsplash.com/fr/photos/une-voiture-peinte-avec-des-personnages-danime-dessus-4aE2enR5M8s');
+        background-image: url('https://imgur.com/gallery/anime-anime-6fBbf');
         background-size: cover;
         background-attachment: fixed;
       }
